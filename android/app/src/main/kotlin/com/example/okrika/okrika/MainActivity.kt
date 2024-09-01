193a119eff2790994383ba2632b277462ec4440b
habyaad <adeomotoshan@gmail.com>
2024-09-01 13:00:44 +0100

    Initial Project Setup

---
+++ android/app/src/main/kotlin/com/example/okrika/okrika/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.okrika.okrika
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:okrika/features/splash/splash_screen.dart';
+import 'features/products/presentation/screens/product_catalogue.dart';
+import 'features/products/presentation/screens/product_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Okrika',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+      routes: {
+        '/product-catalogue': (ctx) => const ProductCatalogue(),
+        '/product-view': (ctx) => const ProductView(),
+      },
+    );
+  }
+}
