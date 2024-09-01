3b2296d181b308751276d71f98e453cfae922ee5
habyaad <adeomotoshan@gmail.com>
2024-09-01 13:04:48 +0100

    feat: create color palette for app

---
+++ lib/features/splash/splash_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:loading_indicator/loading_indicator.dart';
+import 'package:okrika/shared/utils/app_colors.dart';
+
+import '../../shared/widgets/custom_text.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed((const Duration(seconds: 2)), () {
+      Navigator.of(context).pushReplacementNamed('/product-catalogue');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 200,
+            child: LoadingIndicator(
+              indicatorType: Indicator.orbit,
+              colors: [Colors.blueGrey, Colors.redAccent],
+              strokeWidth: 2,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Center(
+            child: CustomText(
+              title: "OKRIKA",
+              size: 32,
+              weight: FontWeight.w700,
+              color: AppColors.appOrange,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/shared/utils/app_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // karot colors
+  static const Color primary500 = Color(0xFF2453AD);
+  static const Color appRed = Color(0xFFB31212);
+  static const Color appGrey = Color(0xFFD9D9D9);
+  static const Color success = Color(0xFFBFFCDD);
+  static const Color appBlue = Color(0xFF002854);
+  static const Color appOrange = Color(0xFFFE8710);
+  static const Color appGreyDark = Color(0xFFB1ADAD);
+  static const Color appGreen = Color(0xFF24AD7C);
+  static const Color appLight50 = Color(0xFFB3B6CB);
+  static const Color appLight40 = Color(0xFFC4C6D7);
+  static const Color appLight30 = Color(0xFFE8E9F0);
+  static const Color appLight20 = Color(0xFFF2F2F8);
+  static const Color appLight100 = Color(0xFFA5A8C0);
+  static const Color appDark700 = Color(0xFF1B1B28);
+  static const Color appDark500 = Color(0xFF373B4D);
+  static const Color placeHolder = Color(0xFFB3B6CB);
+  static const Color medium200 = Color(0xFF868EA3);
+  static const Color medium300 = Color(0xFF616B7C);
+}
