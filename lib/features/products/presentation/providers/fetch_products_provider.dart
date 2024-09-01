4247191a531c15617d46b2e15f367916d4d4c3c1
habyaad <adeomotoshan@gmail.com>
2024-09-01 13:46:25 +0100

    feat: create fetch products class based provider with other methods

---
+++ lib/features/products/presentation/providers/fetch_products_provider.dart
@@ -0,0 +1,38 @@
+import 'package:okrika/features/products/domain/models/category_model.dart';
+import 'package:okrika/features/products/domain/models/product_model.dart';
+import 'package:okrika/features/products/domain/providers/product_catalogue_provider.dart';
+import 'package:okrika/features/products/domain/repositories/product_catalogue_repo.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'fetch_products_provider.g.dart';
+
+@Riverpod()
+class FetchProducts extends _$FetchProducts {
+  @override
+  Future<List<ProductModel>> build() async {
+    final ProductCatalogueRepository productRepo =
+        ref.read(productCatalogueProvider);
+    return await productRepo.fetchProducts();
+  }
+
+  Future<void> setFilter(Map<String, dynamic> filterData) async {
+    final ProductCatalogueRepository productRepo =
+        ref.read(productCatalogueProvider);
+    state = AsyncData(await productRepo.filterProducts(filterData));
+  }
+
+  Future<void> addProduct(Map<String, dynamic> filterData) async {
+    final ProductCatalogueRepository productRepo =
+        ref.read(productCatalogueProvider);
+    state = AsyncData(await productRepo.filterProducts(filterData));
+  }
+
+  Future<void> deleteProduct(int id, Map<String, dynamic>? filterData) async {
+    final ProductCatalogueRepository productRepo =
+        ref.read(productCatalogueProvider);
+    await productRepo.deleteProduct(id);
+    if (filterData != null) {
+      setFilter(filterData);
+    }
+  }
+}
---
+++ lib/features/products/presentation/providers/fetch_products_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'fetch_products_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$fetchProductsHash() => r'd3f41c8b0c3afb5e2f95a6361499a86a138442bc';
+
+/// See also [FetchProducts].
+@ProviderFor(FetchProducts)
+final fetchProductsProvider = AutoDisposeAsyncNotifierProvider<FetchProducts,
+    List<ProductModel>>.internal(
+  FetchProducts.new,
+  name: r'fetchProductsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$fetchProductsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$FetchProducts = AutoDisposeAsyncNotifier<List<ProductModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
