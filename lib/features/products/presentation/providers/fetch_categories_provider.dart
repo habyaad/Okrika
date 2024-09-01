a0dc69c09d54c83b67b80c3831314687f6f4d8cb
habyaad <adeomotoshan@gmail.com>
2024-09-01 13:22:33 +0100

    feat: create fetch categories provider

---
+++ lib/features/products/presentation/providers/fetch_categories_provider.dart
@@ -0,0 +1,13 @@
+import 'package:okrika/features/products/domain/models/category_model.dart';
+import 'package:okrika/features/products/domain/providers/product_catalogue_provider.dart';
+import 'package:okrika/features/products/domain/repositories/product_catalogue_repo.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'fetch_categories_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+Future<List<CategoryModel>> fetchCategories(
+    FetchCategoriesRef ref) async {
+  final ProductCatalogueRepository productRepo = ref.read(productCatalogueProvider);
+  return await productRepo.fetchCategories();
+}
---
+++ lib/features/products/presentation/providers/fetch_categories_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'fetch_categories_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$fetchCategoriesHash() => r'52ec930da427c5f71f7ec04f3ee9fba0a5860db1';
+
+/// See also [fetchCategories].
+@ProviderFor(fetchCategories)
+final fetchCategoriesProvider = FutureProvider<List<CategoryModel>>.internal(
+  fetchCategories,
+  name: r'fetchCategoriesProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$fetchCategoriesHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FetchCategoriesRef = FutureProviderRef<List<CategoryModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
