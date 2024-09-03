92a7778ba34792cf5bda846674238037ee1389e5
habyaad <adeomotoshan@gmail.com>
2024-09-03 10:46:43 +0100

    refactor: replace temporary variable for filter with filter provider

--- lib/features/products/presentation/providers/fetch_products_provider.dart
+++ lib/features/products/presentation/providers/fetch_products_provider.dart
@@ -1,11 +1,12 @@
 import 'package:okrika/features/products/domain/models/product_model.dart';
 import 'package:okrika/features/products/domain/providers/product_catalogue_provider.dart';
 import 'package:okrika/features/products/domain/repositories/product_catalogue_repo.dart';
+import 'package:okrika/features/products/presentation/providers/filter_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'fetch_products_provider.g.dart';
 
-@Riverpod()
+@Riverpod(keepAlive: true)
 class FetchProducts extends _$FetchProducts {
   @override
   Future<List<ProductModel>> build() async {
@@ -14,38 +15,42 @@ class FetchProducts extends _$FetchProducts {
     return await productRepo.fetchProducts();
   }
 
-  Future<void> setFilter(Map<String, dynamic> filterData) async {
+  Future<void> setFilter() async {
     final ProductCatalogueRepository productRepo =
         ref.read(productCatalogueProvider);
+    final Map<String, dynamic> filterData = ref.read(filterProvider);
     state = AsyncData(await productRepo.filterProducts(filterData));
   }
 
   Future<void> addProduct(Map<String, dynamic> productData) async {
     final ProductCatalogueRepository productRepo =
         ref.read(productCatalogueProvider);
+    final Map<String, dynamic> filterData = ref.read(filterProvider);
     try {
       await productRepo.createNewProduct(productData);
-      ref.invalidateSelf();
+      setFilter();
     } catch (e) {
       rethrow;
     }
   }
 
-  Future<void> deleteProduct(int id, Map<String, dynamic>? filterData) async {
+  Future<void> deleteProduct(
+    int id,
+  ) async {
     final ProductCatalogueRepository productRepo =
         ref.read(productCatalogueProvider);
     await productRepo.deleteProduct(id);
-    if (filterData != null) {
-      setFilter(filterData);
-    }
+    setFilter();
   }
 
   Future<void> editProduct(int id, Map<String, dynamic> productData) async {
     final ProductCatalogueRepository productRepo =
         ref.read(productCatalogueProvider);
+    final Map<String, dynamic> filterData = ref.read(filterProvider);
+
     try {
       await productRepo.editProduct(id, productData);
-      ref.invalidateSelf();
+      setFilter();
     } catch (e) {
       rethrow;
     }
