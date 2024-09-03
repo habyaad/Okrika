b5513b708ac8dec97089889098e4676299b1348c
habyaad <adeomotoshan@gmail.com>
2024-09-03 10:45:52 +0100

    feat: implement filter provider

---
+++ lib/features/products/presentation/providers/filter_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'filter_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$filterHash() => r'f8788dc6471d49c1c30b01c450a56a296176f099';
+
+/// See also [Filter].
+@ProviderFor(Filter)
+final filterProvider = NotifierProvider<Filter, Map<String, dynamic>>.internal(
+  Filter.new,
+  name: r'filterProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$filterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Filter = Notifier<Map<String, dynamic>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
